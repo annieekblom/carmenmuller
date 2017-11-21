f25fb0e80b10f5520e2eb0810e60c5669c541620
annieekblom <ekan1600@users.noreply.github.com>
2017-11-21 14:23:02 +0100

    first attempt

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Carmen Müller</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url') ?>">
+  <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
+<?php wp_head(); ?>
+</head>
+
+<header>
+	<h1>This is the header</h1>
+</header>
--- index.php
+++ index.php
@@ -1,4 +1,25 @@
 <?php
 // Silence is golden.
-echo "Hello";
+get_header();
+
 ?>
+<?php
+
+ echo "Hello";
+
+if ( have_posts() ) :
+	while ( have_posts() ) : the_post(); ?>
+
+         <h2><?php the_title() ?></h2>
+		<?php the_content() ?>
+	<?php endwhile;
+ 
+else :
+	echo '<p>There are no posts!</p>';
+ 
+endif;
+ 
+	?>
+?>
+
+<?php get_footer(); ?>
